a20587f5281e734de2ef8488898ed53a9d0e443b
alexbrul <42654943+alexbrul@users.noreply.github.com>
2023-03-15 17:49:44 +0100

    drd da

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ def findAvailableProduct(url):
 
     # Find all <li> elements on the page
     list_items = soup.find_all('li')
-    print(list_items)
-    for li in list_items:
-        print(li)
 
     # Create an empty list to hold the results
     results = []
@@ -20,20 +17,20 @@ def findAvailableProduct(url):
     # Loop through each <li> element
     for li in list_items:
         try:
-            # Find the <picture> and <svg> elements inside the <li>
             picture = li.find('picture')
             svg = li.find_all('svg')
 
-            # Extract the 'alt' text from the <picture> element and the class name from the <svg> element
             alt_text = picture.find('img')['alt']
             class_name = svg[-2]['class'][-1]
 
-            # Add the extracted information to the results list as a tuple
-            results.append((alt_text, class_name))
+            if class_name == "iconstockinstock":
+                results.append((alt_text, class_name))
         except:
             pass
 
     # Print the results list
-    print(results)
+    return results
 
-findAvailableProduct(urll)
+output = findAvailableProduct(urll)
+
+print(output)
